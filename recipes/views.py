ddc9abf457778dde1e1c8ea1bf723ab305d50d60
KokinFR <franciscorafaelzero78@gmail.com>
2025-09-07 15:05:57 -0300

    Transferring views from urls.py to recipes' views.py

--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse('Home')
+
+def sobre(request):
+    return HttpResponse('Sobre')
+
+def contato(request):
+    return HttpResponse('Contato')
