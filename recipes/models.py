244014d13fc6a968d21327b6de19aa234ad00312
KokinFR <franciscorafaelzero78@gmail.com>
2025-11-03 15:11:40 -0300

    feat: Registering category in Django admin.

--- recipes/models.py
+++ recipes/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Category(models.Model):
     name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
 
 class Recipe(models.Model):
     title = models.CharField(max_length=50)
