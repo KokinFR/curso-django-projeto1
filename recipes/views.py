cc1ac4d6da4a16cccaaebe210b4f42e80bd2da79
KokinFR <franciscorafaelzero78@gmail.com>
2025-11-05 14:32:29 -0300

    feat: Showing error 404 Not Found for pages that do not exist.

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from ultis.recipes.factory import make_recipe
 from recipes.models import Recipe
+from django.http import Http404
 
 # Create your views here.
 def home(request):
@@ -11,8 +12,13 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+
+    if not recipes:
+        raise Http404('Not found')
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category | '
     })
 
 def recipe(request, id):
