1d7e7ae6507dab695b99357c9db7b05cdd5a65cb
KokinFR <franciscorafaelzero78@gmail.com>
2025-11-19 14:45:07 -0300

    feat: # noqa

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,13 +1,13 @@
 from django.test import TestCase
 from django.urls import reverse
 
-class RecipeURLsTest(TestCase):
+class RecipeURLsTest(TestCase):  # noqa E302
     def test_recipe_home_url_is_correct(self):
         url = reverse('recipes:home')
         self.assertEqual(url, '/')
         
     def test_recipe_category_url_is_correct(self):
-        url = reverse('recipes:category', kwargs={ 'category_id': 1})
+        url = reverse('recipes:category', kwargs={ 'category_id': 1}) # noqa E501
         self.assertEqual(url, '/recipes/category/1/')
 
     def test_recipe_detail_url_is_correct(self):
