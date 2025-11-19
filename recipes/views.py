a3f48c447df199680cda1ac5cc07af4b263116ae
KokinFR <franciscorafaelzero78@gmail.com>
2025-11-19 15:03:36 -0300

    feat: RecipeViewsTest

--- recipes/views.py
+++ recipes/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-from ultis.recipes.factory import make_recipe
 from recipes.models import Recipe
 
 # Create your views here.
-def home(request):
+def home(request):# noqa E302
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
-def category(request, category_id):
-    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id'))
+def category(request, category_id):# noqa E302
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id, 
+            is_published=True).order_by('-id'))
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
         'title': f'{recipes[0].category.name} - Category | '
     })
 
-def recipe(request, id):
+def recipe(request, id): # noqa E302
     recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
 
     return render(request, 'recipes/pages/recipe-view.html', context={
