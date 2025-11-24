182254c4331551b249a64563f9db55ed6ed07099
KokinFR <franciscorafaelzero78@gmail.com>
2025-11-24 14:57:17 -0300

    feat: Testing the context and content of the response in the template.

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
-
+from recipes.models import Recipe, Category, User
 
 class RecipeViewsTest(TestCase): # noqa E302
+    # Test Home
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
@@ -23,6 +24,32 @@ class RecipeViewsTest(TestCase): # noqa E302
             response.content.decode('utf-8')
         )
 
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name='Category')
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='username@email.com',
+        )
+        recipe = Recipe.objects.create(
+            category=category,
+            author=author,
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutes',
+            servings=5,
+            servings_unit='Portions',
+            preparation_steps='Recipe Preparation Steps',
+            preparation_steps_is_html=False,
+            is_published=True,
+        )
+        assert 1 == 1
+
+    #Test Category
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(
             reverse('recipes:category', kwargs={ 'category_id': 1000})
@@ -34,7 +61,8 @@ class RecipeViewsTest(TestCase): # noqa E302
             reverse('recipes:category', kwargs={ 'category_id': 1000})
         )
         self.assertEqual(response.status_code, 404)
-    
+
+    # Test Recipe Detail    
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(
             reverse('recipes:recipe', kwargs={'id': 1})
